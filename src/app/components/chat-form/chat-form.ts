c76b99e26213e30f8206fbb9c8ea5bd319820c4e
Ariana <136492482+aryins@users.noreply.github.com>
2026-03-20 13:24:06 -0300

    Delete src directory

--- src/app/components/chat-form/chat-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-chat-form',
-  imports: [],
-  templateUrl: './chat-form.html',
-  styleUrl: './chat-form.css',
-})
-export class ChatForm {}
